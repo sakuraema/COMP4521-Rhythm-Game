b822202ef61d8825f83c48080f81dc1625af73d5
sakuraema <tonychu122003@gmail.com>
2021-05-04 21:43:11 +0900

    Add scroller

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.PlayDelayed(5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public AudioSource musicSource;
+}
---
+++ Assets/Scripts/Scroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_MovementSpeed = BPM;
+        transform.position = new Vector3 (0, 0, 670);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0.0f, 0.0f, m_MovementSpeed * Time.deltaTime * GameManager.speedOffset);
+    }
+
+    public int BPM;
+    private float m_MovementSpeed;
+}

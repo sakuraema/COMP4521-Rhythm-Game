c972b36d18b29d91b85cfd3cc7fdd28d8c9001b6
Xelopie <47631876+Xelopie@users.noreply.github.com>
2021-05-09 02:42:02 +0800

    Correct beat

--- Assets/Scripts/ScrollerEditor.cs
+++ Assets/Scripts/ScrollerEditor.cs
@@ -2,22 +2,22 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditorInternal;
 
-[CustomEditor(typeof(Scroller))]
-public class ScrollerEditor : Editor
+[CustomEditor(typeof(Beatmap))]
+public class BeatmapEditor : Editor
 {
-	//private ReorderableList list;
+	private ReorderableList list;
 
-	//private void OnEnable()
-	//{
-	//	list = new ReorderableList(serializedObject,
-	//			serializedObject.FindProperty("beats"),
-	//			true, true, true, true);
-	//}
+	private void OnEnable()
+	{
+		list = new ReorderableList(serializedObject,
+				serializedObject.FindProperty("beats"),
+				true, true, true, true);
+	}
 
-	//public override void OnInspectorGUI()
-	//{
-	//	serializedObject.Update();
-	//	list.DoLayoutList();
-	//	serializedObject.ApplyModifiedProperties();
-	//}
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+		list.DoLayoutList();
+		serializedObject.ApplyModifiedProperties();
+	}
 }
